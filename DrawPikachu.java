0b72ff6e90967adc428432354e0198e7c1f6a7dd
tuluibattulga <169181738+tuluibattulga@users.noreply.github.com>
2024-05-07 09:23:28 -0700

    Add files via upload
    creates a text art pikachu

---
+++ DrawPikachu.java
@@ -0,0 +1,18 @@
+package pikachu;
+/**
+ * Prints an intro to the pokemon world and a text art of pikachu.
+ * 
+ * @author (Tului Battulga) 
+ * @version (8/30/23)
+ */
+
+public class DrawPikachu {
+
+	public static void main(String[] args) {
+		System.out.println("   Pikachu welcomes you to the world of Pokemon!\n");
+		System.out.println("                      (\\__/)");
+		System.out.println("                      (o^.^)");
+		System.out.println("                    z(_(\")(\")");
+	
+	}
+}
